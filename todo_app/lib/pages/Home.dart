283e33f16b97f131a95d6c83303a0cdcb29fd57e
Mahesh-xr <mahesh.572xr@gmail.com>
2025-05-23 09:39:39 +0530

    create structure

---
+++ todo_app/lib/pages/Home.dart
@@ -0,0 +1,56 @@
+import "package:flutter/material.dart";
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top:50, left:30),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF232FDA2), Color(0xFF13D8CA), Color(0xFF09adfe)],begin: Alignment.topLeft, end: Alignment.bottomRight)),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hello\nMahesh",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+
+            ),
+            ),
+            Row(
+              children: [
+                Material(
+                  elevation: 10,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal:20, vertical: 5),
+                    decoration: BoxDecoration(color:Color(0xFF3dffe3),borderRadius:BorderRadius.circular(20) , ),          
+                              child: Text("Today", 
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 249, 246, 246)
+                    
+                    ),),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
