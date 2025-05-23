c789a42484d15656dc45f7e3ee414798ea171899
Mahesh-xr <mahesh.572xr@gmail.com>
2025-05-23 11:05:33 +0530

    add sized box

--- todo_app/lib/pages/Home.dart
+++ todo_app/lib/pages/Home.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -13,41 +12,60 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top:50, left:30),
+        padding: EdgeInsets.only(top: 50, left: 30),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF232FDA2), Color(0xFF13D8CA), Color(0xFF09adfe)],begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF232FDA2), Color(0xFF13D8CA), Color(0xFF09adfe)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
 
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Hello\nMahesh",
-            style: TextStyle(
-              fontSize: 30,
+            Text(
+              "Hello\nMahesh",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text("Good Morning",
+            style:TextStyle(
               color: Colors.white,
-              fontWeight: FontWeight.bold
-
-            ),
-            ),
+              fontWeight: FontWeight.w300,
+              
+              fontSize: 20,
+            ) ,),
+            SizedBox(height:10),
             Row(
               children: [
                 Material(
                   elevation: 10,
                   borderRadius: BorderRadius.circular(20),
                   child: Container(
-                    padding: EdgeInsets.symmetric(horizontal:20, vertical: 5),
-                    decoration: BoxDecoration(color:Color(0xFF3dffe3),borderRadius:BorderRadius.circular(20) , ),          
-                              child: Text("Today", 
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 249, 246, 246)
-                    
-                    ),),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF3dffe3),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      "Today",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color:Colors.white,
+                      ),
+                    ),
                   ),
-                )
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
